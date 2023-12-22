cbb530f4772ee6f4e3d411cfad50217269132cc2
kosmokit221 <144554376+kosmokit221@users.noreply.github.com>
2023-12-22 19:32:41 +0200

    Add files via upload

---
+++ EasyEditor.py
@@ -0,0 +1,46 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow,QFileDialog
+from ui import Ui_MainWindow
+import os
+class EasyEditor(QMainWindow):
+    def   __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.workdir = None
+        self.filename = None
+        self.connects()
+
+    def connects(self):
+        self.ui.folder_btn.clicked.connect(self.choose_folder)
+
+    def filter(self, filenames):
+        images = []
+        for filename in filenames:
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                images.append(filename)
+
+        return images        
+
+
+    def choose_folder(self):
+        try:
+            self.workdir = QFileDialog.getExistingDirectory()
+            self.show_image_list()
+        except:
+            pass
+
+        
+        
+    def show_image_list(self):
+        filenames = os.listdir(self.workdir)
+        self.ui.listWidget.clear()
+        images = self.filter(filenames)
+        self.ui.listWidget.addItems(images)
+
+        
+
+app = QApplication([])
+ex = EasyEditor()
+ex.show()
+app.exec_()
